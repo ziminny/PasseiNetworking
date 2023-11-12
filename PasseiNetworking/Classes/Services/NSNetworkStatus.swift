fc9d2daf61a93251cb434f09f5413e1ee2b4bbc0
Vagner Oliveira <ziminny@gmail.com>
2023-11-12 12:39:30 -0300

    no message

---
+++ PasseiNetworking/Classes/Services/NSNetworkStatus.swift
@@ -0,0 +1,46 @@
+//
+//  NSNetworkStatus.swift
+//
+//
+//  Created by Vagner Oliveira on 02/06/23.
+//
+
+public class NSNetworkStatus:ObservableObject {
+
+    @Published public var isConnected = true
+    
+    public init(queue:DispatchQueue = .main) {
+        let monitor = NWPathMonitor()
+        
+        let localQueue:((_ callback: @escaping () -> Void) -> Void) = { callback in
+            if queue == Thread.main {
+                DispatchQueue.main.async {
+                    callback()
+                }
+            } else {
+                DispatchQueue.global().sync {
+                   callback()
+                }
+            }
+        }
+   
+        let semaphore = DispatchSemaphore(value: 0)
+
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                localQueue { self.isConnected = true }
+            } else {
+                localQueue { self.isConnected = false }
+            }
+
+            semaphore.signal()
+        }
+
+        let queue = DispatchQueue(label: "networkMonitor")
+        monitor.start(queue: queue)
+
+        semaphore.wait()
+
+        
+    }
+}
