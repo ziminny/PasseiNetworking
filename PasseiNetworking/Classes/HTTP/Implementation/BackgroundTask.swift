3e7df9249f581ce4ac4590e9b5d072441b5c1cdb
Vagner Oliveira <ziminny@gmail.com>
2024-01-30 18:48:28 -0300

    background tasks

--- PasseiNetworking/Classes/HTTP/Implementation/BackgroundTask.swift
+++ PasseiNetworking/Classes/HTTP/Implementation/BackgroundTask.swift
@@ -10,13 +10,13 @@
 import Foundation
 
 public extension URLSessionConfiguration {
-    /// Tarefas demoradas em segundo plano
-    static var timeConsumingBackgroundTasks:URLSessionConfiguration {
+    /// Tarefas demoradas em segundo plano (Não pode usar dados moveis)
+    static var timeConsumingBackgroundTasksNo3GAccess: URLSessionConfiguration {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = HTTPHeader.headerDict
         configuration.waitsForConnectivity = true
-        configuration.timeoutIntervalForRequest = 60 // 1 Minuto
-        configuration.timeoutIntervalForResource = 60 * 60 // 1 Hora
+        configuration.timeoutIntervalForRequest = 3600 // 1 hora
+        configuration.timeoutIntervalForResource = 60 * 60 * 3 // 3 Hora
         // Não pode usar 3G
         configuration.allowsCellularAccess = false
         // Vai ser requisição pesada em segundo plano
@@ -28,32 +28,50 @@ public extension URLSessionConfiguration {
 }
 
 public extension URLSessionConfiguration {
-    /// Tarefas medias em segundo plano
-    static var averangeBackgroundTasks:URLSessionConfiguration {
+    /// Tarefas demoradas em segundo plano (Pode usar dados moveis)
+    static var timeConsumingBackgroundTasks: URLSessionConfiguration {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = HTTPHeader.headerDict
         configuration.waitsForConnectivity = true
-        configuration.timeoutIntervalForRequest = 45 // 45 Segundos
-        configuration.timeoutIntervalForResource = 60 * 60 / 4 // 15 Minutos
-        // Não pode usar 3G
-        configuration.allowsCellularAccess = false
-        // Vai ser requisição media em segundo plano
-        configuration.allowsExpensiveNetworkAccess = false // (Requisições considerada caras)
-        // Vai ser requisição media em segundo plano (desabilitar)
-        configuration.allowsConstrainedNetworkAccess = false // (Low Data Model)
+        configuration.timeoutIntervalForRequest = 3600 // 1 hora
+        configuration.timeoutIntervalForResource = 60 * 60 * 3 // 3 Hora
+        // Pode usar 3G
+        configuration.allowsCellularAccess = true
+        // Vai ser requisição pesada em segundo plano
+        configuration.allowsExpensiveNetworkAccess = true // (Requisições considerada caras)
+        // Vai ser requisição pesada em segundo plano (desabilitar)
+        configuration.allowsConstrainedNetworkAccess = true // (Low Data Model)
         return configuration
     }
 }
 
 public extension URLSessionConfiguration {
-    /// Tarefas leves em segundo plano
-    static var lightBackgroundTasks:URLSessionConfiguration {
+    /// Tarefas medias em segundo plano (Pode usar dados moveis)
+    static var averangeBackgroundTasks: URLSessionConfiguration {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = HTTPHeader.headerDict
+        configuration.waitsForConnectivity = true
+        configuration.timeoutIntervalForRequest = 1800  // 30 Minutos
+        configuration.timeoutIntervalForResource = 60 * 60// 1 Hora
+        // Pode usar 3G
+        configuration.allowsCellularAccess = true
+        // Vai ser requisição media em segundo plano
+        configuration.allowsExpensiveNetworkAccess = true // (Requisições considerada caras)
+        // Vai ser requisição media em segundo plano (desabilitar)
+        configuration.allowsConstrainedNetworkAccess = true // (Low Data Model)
+        return configuration
+    }
+}
+
+public extension URLSessionConfiguration {
+    /// Tarefas leves em segundo plano (Pode usar dados moveis)
+    static var lightBackgroundTasks: URLSessionConfiguration {
         
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = HTTPHeader.headerDict
         configuration.waitsForConnectivity = true
-        configuration.timeoutIntervalForRequest = 20 // 20 Segundos
-        configuration.timeoutIntervalForResource = 60 * 3 // 3 Minutos
+        configuration.timeoutIntervalForRequest = 900 // 15 minutos
+        configuration.timeoutIntervalForResource = 60 * 60 / 3 // meia Hora
         // Pode usar 3G
         configuration.allowsCellularAccess = true
         // Vai ser requisição leve mas em segundo plano
@@ -67,7 +85,7 @@ public extension URLSessionConfiguration {
 
 public extension URLSessionConfiguration {
     /// Tarefa Principal, ex:. Login, Criar conta
-    static var noBackgroundTask:URLSessionConfiguration {
+    static var noBackgroundTask: URLSessionConfiguration {
         
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = HTTPHeader.headerDict
