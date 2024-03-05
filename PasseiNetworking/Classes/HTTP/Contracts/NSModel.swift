712b67ea98d74ed0f08dd33c7c03393a989c00c7
Vagner Oliveira <ziminny@gmail.com>
2024-03-05 17:46:42 -0300

    remove updatedAt NSModel

--- PasseiNetworking/Classes/HTTP/Contracts/NSModel.swift
+++ PasseiNetworking/Classes/HTTP/Contracts/NSModel.swift
@@ -12,15 +12,4 @@ import Foundation
 ///            class MyModel:NSModel {
 ///            }
 ///      ```
-public protocol NSModel where Self: Decodable & Encodable {
-    var updatedAt:Date { get set }
-//    var createdAt:Date { get set }
-}
- 
-public extension NSModel {
-    var updatedAt:Date { get { Date() } set {} }
-//    var createdAt:Date { get { Date() } set {} }
-    
-//    TO DO: Pensar numa melhor maneira de passar o createdAt do swift para o backEnd para fins de relatórios, filtros (exemplo: puxar algo por data de criação).
-}
-
+public protocol NSModel where Self: Decodable & Encodable { } 
